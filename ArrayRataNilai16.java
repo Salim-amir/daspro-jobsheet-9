88f77e5b2fea3f894fb4df521eb6171ca313b2a3
Salim-amir <salimamir2005@gmail.com>
2024-10-29 12:17:21 +0700

    Percobaan 3

---
+++ ArrayRataNilai16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayRataNilai16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+     int[] Nilaimhs = new int[10];
+     double total = 0;
+     double rata2 ;
+
+     for (int i = 0; i < Nilaimhs.length; i++) {
+        System.out.print("Masukkan nilai akhir ke-"+(i+1)+" : ");
+        Nilaimhs[i] = sc.nextInt();
+    }
+    for (int i = 0; i < Nilaimhs.length; i++){
+        total += Nilaimhs[i];
+    }
+    rata2 = total/Nilaimhs.length;
+    System.out.println("Rata-Rata Nilai = " + rata2);
+    }
+}
